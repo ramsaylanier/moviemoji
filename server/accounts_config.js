8eb34c34fa4fe411d4642b24bb45fa604a277f12
Ramsay Lanier <ramsay@nclud.com>
2015-05-11 13:27:08 -0400

    added giphy attribution

--- server/accounts_config.js
+++ server/accounts_config.js
@@ -5,10 +5,8 @@ Accounts.config(function(){
 Accounts.validateNewUser(function(user){
 	var existingUsername = Meteor.users.findOne({username: user.username});
 
-	console.log(user.services.facebook.email);
-	console.log(existingUsername);
-
 	if (existingUsername){
+		if (user.services.facebook.email)
 		throw new Meteor.Error(403, "Username already exists");
 	} else if (user.username >= 4){
 		throw new Meteor.Error(403, "Username must have at least 4 characters");
@@ -27,7 +25,6 @@ Accounts.onCreateUser(function(options, user){
 			user.username = user.services.facebook.name;
 		}
 	}
-
-
+	
 	return user;
 })
